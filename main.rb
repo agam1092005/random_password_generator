611dd3fea7b2c1ca6b80a6687e513b644e794469
Agampreet Singh <agam1092005@gmail.com>
2024-03-29 22:42:44 +0530

    Done

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + ['!', '@', '#', '$', '%', '^', '&', '*']
 
-puts "Enter the length of the password: "
+puts "Length of password: "
 length = gets.chomp.to_i
 
 password = Array.new(length) { chars.sample }.join
